41e6d6b12095fa5e0b63eddf88c1d6b91465c6dc
vexra <unique123wowros@gmail.com>
2023-05-15 11:39:51 +0700

    Update theme pt 1

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ require "function.php";
 <body>
     <!-- Spinner Start -->
     <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-        <div class="spinner-grow text-primary" role="status"></div>
+        <div class="spinner-grow" style="background-color: #01C38D" role="status"></div>
     </div>
     <!-- Spinner End -->
 
